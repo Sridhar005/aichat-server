df23e556b59be368e00a1629b747269cd4a2e103
TekTS-16 <TekTS-16@TEK-PWC19>
2026-05-08 01:12:15 -0700

    add new model

---
+++ Models/SendMessageResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp.Models
+{
+    public class SendMessageResponse
+    {
+
+        public Guid ChatId { get; set; }
+        public string ChatTitle { get; set; } = string.Empty;
+        public string Reply { get; set; } = string.Empty;
+
+    }
+}
